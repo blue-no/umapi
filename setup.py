08d0764da5e0a38046a08fdbf09a7e67b9a81b21
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-05-11 16:02:39 +0900

    Add json files

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="umapi",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     description="Tool for monitoring and controlling printer via Ultimaker Api",
     author="Kota Aono",
     url="https://github.com/ut-hnl-lab/umapi.git",
+    package_data={"": ["*.json"]},
     packages=find_packages()
 )
